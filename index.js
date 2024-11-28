17be24bdc27471bec629ff32d840519562bfcb6e
Camila <camilinha.cm59@gmail.com>
2024-11-28 10:20:34 -0300

    tirei o vercel

--- index.js
+++ index.js
@@ -14,3 +14,7 @@ app.listen(porta, () => {
     console.log(`Servidor está ouvindo na porta ${porta}...`);
 });
 
+app.get('/', (req, res) => {
+    res.send('API do Microserviço de Notas funcionando!');
+});
+
