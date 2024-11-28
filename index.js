a46a7fda1037fd2814413b1a41d4f1a0cecc9674
Camila <camilinha.cm59@gmail.com>
2024-11-28 09:48:16 -0300

    mudancas vercel

--- index.js
+++ index.js
@@ -13,3 +13,8 @@ const porta = process.env.PORT || 3000;
 app.listen(porta, () => {
     console.log(`Servidor está ouvindo na porta ${porta}...`);
 });
+
+app.get('/', (req, res) => {
+    res.send('API do Microserviço de Notas funcionando!');
+});
+
